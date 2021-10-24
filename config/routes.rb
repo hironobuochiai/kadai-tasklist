e0dea7b5971115d8a742b233218f32fc5f1270cf
hironobu.ochiai <hironobuochiai113@gmail.com>
2021-10-24 13:20:08 +0000

    kadai-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root to: 'tasks#index'
  
  resources :tasks
  resources :users, only: [:new, :create]
- 
- get 'login', to: 'session#new'
- post 'login', to: 'session#create'
- 
+ resources :sessions, only: [:new, :create, :destroy]
 
 end
